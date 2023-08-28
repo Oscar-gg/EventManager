74e914d817f930fdf7d6706f9c94ca9908172554
Oscar-gg <oscar.arreola.jr@gmail.com>
2023-08-28 13:28:05 -0600

    Test github orgs with null values

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -2,7 +2,10 @@ import Layout from "~/components/layout/Layout";
 import BarViewEvents from "~/components/containers/BarViewEvents";
 import { PageBody } from "~/components/general/PageElements";
 
+import { getUserOrganizations } from "~/server/AuthRoles";
+
 export default function Home() {
+  test();
   return (
     <Layout>
       <PageBody>
@@ -11,3 +14,13 @@ export default function Home() {
     </Layout>
   );
 }
+
+const test = async () => {
+  console.log("Running test:");
+
+  const organizations = await getUserOrganizations("https://api.github.com/users/afr2903/orgs");
+  console.log(organizations);
+  const roborregos = organizations.find((org) => org.login === "RoBorregos");
+  console.log(roborregos);
+  if (roborregos) return true;
+};
