f81868db3e07a013ed2b29cc1819300c5bbeb76b
Oscar-gg <oscar.arreola.jr@gmail.com>
2024-02-25 18:50:48 -0600

    Commented test function

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -2,7 +2,7 @@ import Layout from "~/components/layout/Layout";
 import BarViewEvents from "~/components/containers/BarViewEvents";
 import { PageBody } from "~/components/general/PageElements";
 
-import { getUserOrganizations } from "~/server/AuthRoles";
+// import { getUserOrganizations } from "~/server/AuthRoles";
 
 export default function Home() {
   test();
@@ -15,12 +15,12 @@ export default function Home() {
   );
 }
 
-const test = async () => {
+const test = () => {
   console.log("Running test:");
-
-  const organizations = await getUserOrganizations("https://api.github.com/users/afr2903/orgs");
-  console.log(organizations);
-  const roborregos = organizations.find((org) => org.login === "RoBorregos");
-  console.log(roborregos);
-  if (roborregos) return true;
+  
+  // const organizations = await getUserOrganizations("https://api.github.com/users/afr2903/orgs");
+  // console.log(organizations);
+  // const roborregos = organizations.find((org) => org.login === "RoBorregos");
+  // console.log(roborregos);
+  // if (roborregos) return true;
 };
